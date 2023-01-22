2251d7c01b7af5ffde2d033ce52339ffdaf09a88
Rahul Panchal <rahulpanchaloff@hotmail.com>
2023-01-23 00:14:18 +0530

    project one commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+
+import videoPlayer from "../../Assets/Projects/videoPlayer.png";
+import leaveApp from "../../Assets/Projects/leaveApp.png";
+import eventWeb from "../../Assets/Projects/eventWeb.png";
+import wordle from "../../Assets/Projects/wordle.png";
 
 function Projects() {
   return (
@@ -29,20 +33,61 @@ function Projects() {
             />
           </Col>
 
+          {/* Wordle */}
+
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={wordle}
               isBlog={false}
-              title="Notification and Pop-up bar for asset management Website"
-              description="Made Notification and Pop-Up slider for asset management website which deal with crypto using JavaScript and other. Completed this task for satisfied Nigerian Client remotely."
-              ghLink="#"
-              demoLink="fp-asset.com"
+              title="Wordle Clone - NY Times"
+              description="JavaScript project- Clone of New York Times's Wordle game. Used Vanilla JavaScript and CSS for styling."
+              ghLink="https://github.com/rrahulppanchal"
+              demoLink="https://javascript-wordle.vercel.app/"
+            />
+          </Col>
+
+          {/* eventweb */}
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={eventWeb}
+              isBlog={false}
+              title="Front End of Event Web"
+              description="ReactJS project- Event booking and creating website using ReactJS and SCSS, react and others. also worked on Back-End using NodeJS and mySQl."
+              ghLink="https://github.com/rrahulppanchal"
+              demoLink="https://effulgent-otter-7e2ec7.netlify.app/"
+            />
+          </Col>
+
+          {/* leave app*/}
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaveApp}
+              isBlog={false}
+              title="Front End of Leave App"
+              description="ReactJS project- Blog website which let employee of a particular company request to leaves and the management have to approve or decline it."
+              ghLink="https://github.com/rrahulppanchal"
+              demoLink="https://gleaming-crepe-0dd8ab.netlify.app/"
+            />
+          </Col>
+
+          {/* video player */}
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={videoPlayer}
+              isBlog={false}
+              title="Video Player Project"
+              description="JavaScript project- Clone of Youtube video player with same functionality. Used Vanilla JavaScript and CSS for styling."
+              ghLink="https://github.com/rrahulppanchal"
+              demoLink="https://video-player-javascript-nine.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={bitsOfCode}
               isBlog={false}
               title="Front End of Blog Website"
               description="ReactJS project- Blog website which let anyone write blog posts comment and react to them using ReactJS and SCSS and others. also working on Back-End where using NodeJS and mySQl."
